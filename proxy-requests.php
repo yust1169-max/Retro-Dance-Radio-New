9bb0e37b1fcede682eeeb4f72f4feb4be78685b1
yust1169-max <yust1169@gmail.com>
2025-12-10 14:18:33 +0100

    Delete proxy-requests.php

--- proxy-requests.php
+++
@@ -1,5 +0,0 @@
-<?php
-$requests_url = "https://radio.retrodanceradio.com/public/retro_dance_radio/embed-requests";
-header("Access-Control-Allow-Origin: *");
-echo file_get_contents($requests_url);
-?>
