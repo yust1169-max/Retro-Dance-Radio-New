4c69203233c7f6f80a6e2397fd053bccc4aefda4
yust1169-max <yust1169@gmail.com>
2025-12-10 14:19:13 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installed');
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
